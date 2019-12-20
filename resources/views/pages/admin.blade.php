22d6728383fd1982986cddebfa1a0e0dc11a1dc2
Hilkiah Makemo <hilkiah.makemo@skywest.com>
2019-12-19 22:55:22 -0700

    Updated admin view

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.main')
 @section('content')
 
+<ul>
+@foreach ($SiteTree as $tree
+   <li>{{ $tree->id }} {{ $tree->uri }} {{ $tree->url }}</li>
+@endforeach
+</ul>
+<form action="/admin/pages">
+@csrf
+<label for="title">Title</label>
+<input type="text" name="title" id="title" placeholder="title">
+</form>
 @endsection
