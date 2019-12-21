5cfee6b7a5545ff52dff3047924f4680325fca3c
Hilkiah Makemo <hilkiah.makemo@skywest.com>
2019-12-21 09:03:47 -0700

    Updated assets

---
+++ resources/js/core.js
@@ -0,0 +1,36 @@
+import Promise from 'es6-promise';
+try {
+    /**
+     * Common JS libraries
+     */
+    window._ = require('lodash');
+    window.$ = window.jQuery = require('jquery');
+    window.Popper = require('popper.js');
+    require('bootstrap');
+
+    /**
+     * Axios
+     */
+
+    window.Axios = require('axios');
+    window.Axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    window.Axios.defaults.headers.common['X-Crsf-Token'] = document.querySelector('meta[name="crsf-token"]');
+
+    /**
+     * Echo exposes an expressive API for subscribing to channels and listening
+     * for events that are broadcast by Laravel. Echo and event broadcasting
+     * allows your team to easily build robust real-time web applications.
+     */
+
+    let Echo = require('laravel-echo').default;
+    window.Pusher = require('pusher-js');
+
+    window.Echo = new Echo({
+        broadcaster: 'pusher',
+        key: process.env.MIX_PUSHER_APP_KEY,
+        cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+        forceTLS: true
+    });
+} catch (error) {
+    console.error(error);
+}
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? '' }}</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}" />
 </head>
 <body>
 <div id="app">@yield('content')</div>
+<script src="{{ asset('js/main.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/admin.js') }}"></script>
 </body>
 </html>
